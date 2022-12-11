bd29272d892b62419414c88b07c6ccf8a8afe2a0
sanam1060 <sanam_g1990@yahoo.com>
2022-12-11 11:12:48 -0500

    update api/index.js and add home-routes.js to controllers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,25 @@
 const router = require("express").Router();
 
-const commentRoutes = require("./comment-routes");
-const postRoutes = require("./post-routes");
-const userRoutes = require("./user-routes");
+const apiRoutes = require("./api/");
+const homeRoutes = require("./home-routes.js");
+const welcomePage = require("./welcome-page.js");
+const dashboardRoutes = require("./dashboard-routes.js");
+const loginRoutes = require("./login-routes.js");
+const chatRoutes = require("./chat-routes.js");
+const singlePostRoutes = require("./single-post-routes.js");
+// const editCustomerRoutes = require('./edit-customer-routes.js');
 
-router.use("/posts", postRoutes);
-router.use("/users", userRoutes);
-router.use("/comments", commentRoutes);
+router.use("/welcome-page", welcomePage);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/login", loginRoutes);
+router.use("/chat", chatRoutes);
+router.use("/single-post", singlePostRoutes);
+// router.use('/edit-customer', editCustomerRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, Staff, Comment, Customer, Liked } = require("../models");
+
+// if click on / while loggedIn send to welcome page
+router.get("/", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("welcome-page");
+});
+
+// if click on /welcome-page while loggedIn send to dashboard
+router.get("/welcome-page", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("welcome-page");
+});
+
+// get login page
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
+});
+
+// // get login page
+// router.get('/login', (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect('/dashboard/' + req.session.staff_id);
+//     return;
+//   }
+
+//   res.render('login');
+// });
+
+module.exports = router;
