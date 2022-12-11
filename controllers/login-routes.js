41a33ff1b03f6cdeb86bc8c2f2e39108a6103cae
sanam1060 <sanam_g1990@yahoo.com>
2022-12-11 11:15:06 -0500

    add login-routes.js to controllers

---
+++ controllers/login-routes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+// get login page
+router.get("/", (req, res) => {
+  res.render("login");
+});
+
+module.exports = router;
